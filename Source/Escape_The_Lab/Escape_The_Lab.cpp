7f924963ca9d6909658a2d8aec2f0a75ae0a8a5d
ThAb <117899380+ThomasAbate@users.noreply.github.com>
2024-12-02 21:33:59 +0100

    [fix] project

--- Source/Escape_The_Lab/Escape_The_Lab.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Escape_The_Lab.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Escape_The_Lab, "Escape_The_Lab" );
